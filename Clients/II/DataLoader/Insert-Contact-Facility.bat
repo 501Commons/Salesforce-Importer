1cab80774158a1c0f88809303ba79dc3658a7677
Dave Boyce <DaveB@501Commons.org>
2018-01-31 07:28:30 -0800

    updates for excel

--- Clients/II/DataLoader/Insert-Contact-Facility.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Program Files (x86)\salesforce.com\Data Loader\bin
-call process C:\repo\Salesforce-Importer-Private\Clients\%2\Salesforce-Importer\Clients\%2\DataLoader\ Insert-Contact-Facility_%1
