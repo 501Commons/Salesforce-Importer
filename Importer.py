9e06c0fdd9e49c2d22c2aaa933faf69afef9d377
Dave Boyce <daveb@501commons.org>
2018-05-27 07:11:04 -0700

    testing

--- Importer.py
+++ Importer.py
@@ -5,6 +5,10 @@ def main():
     import sys
     from os.path import join
 
+    print('Importer Startup1', file=open('file.txt', 'w'))
+    print('Importer Startup2')
+    sys.stdout.write('Importer Startup3')
+    
     salesforce_type = str(sys.argv[1])
     client_type = str(sys.argv[2])
     client_subtype = str(sys.argv[3])
