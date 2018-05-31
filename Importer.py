04370b96008a97da2d788d1cdcb41d930da1c82f
Dave Boyce <daveb@501commons.org>
2018-05-31 09:18:45 -0700

    update path

--- Importer.py
+++ Importer.py
@@ -297,7 +297,7 @@ def export_dataloader(importer_directory):
     from subprocess import Popen, PIPE
 
     exporter_directory = importer_directory.replace("Importer", "Exporter")
-    bat_file = join(exporter_directory, "..\\..\\..\\exporter.bat")
+    bat_file = join(exporter_directory, "\\..\\..\\..\\exporter.bat")
 
     return_code = ""
     return_stdout = ""
