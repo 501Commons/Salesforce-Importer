4d63c3b342f3bc521590916d1bee2308cb9b19df
Dave Boyce <daveb@501commons.org>
2018-07-02 19:21:00 -0700

    update

---
+++ Clients/II/importer.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM IMPORT_ENVIRONMENT can be Prod or Sandbox
+set IMPORT_ENVIRONMENT=Prod
+
+set IMPORT_WAITTIME=-waittime 300
+set IMPORT_NOUPDATE=
+set IMPORT_NOEXPORTODBC=-noexportodbc
+set IMPORT_NOEXPORTSF=-noexportsf
+set IMPORT_INSERTATTEMPTS=
+
+call "%IMPORTER_DIRECTORY%\..\importer.bat" 
