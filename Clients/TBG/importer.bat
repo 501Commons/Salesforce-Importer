05d6c8f4a0288cf566a537a660dccf986e746565
Dave Boyce <daveb@501commons.org>
2018-10-05 13:23:09 -0700

    updates for tbg

--- Clients/TBG/importer.bat
+++ Clients/TBG/importer.bat
@@ -1,10 +1,10 @@
 @echo off
 
 set IMPORT_WAITTIME=-waittime 300
-set IMPORT_NOUPDATE=
+set IMPORT_NOUPDATE=-noupdate
 set IMPORT_NOEXPORTODBC=-noexportodbc
 set IMPORT_NOEXPORTSF=-noexportsf
-set IMPORT_INSERTATTEMPTS=
 set IMPORT_INTERACTIVEMODE=-interactivemode
+set IMPORT_INSERTATTEMPTS=-insertattempts 1
 
 call "%IMPORTER_DIRECTORY%\..\importer.bat" 
