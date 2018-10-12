ccbe0e585c4d7fe5626a78c33f395a938d80c1e1
Dave Boyce <daveb@501commons.org>
2018-10-12 00:12:25 -0700

    updates

--- importer.py
+++ importer.py
@@ -1,4 +1,77 @@
 """import Module for Excel to Salesforce"""
+global IS_WINDOWS
+
+IS_WINDOWS = False
+try:
+    from win32api import STD_INPUT_HANDLE
+    from win32console import GetStdHandle, KEY_EVENT, ENABLE_ECHO_INPUT, ENABLE_LINE_INPUT, ENABLE_PROCESSED_INPUT
+    IS_WINDOWS = True
+except ImportError as e:
+    import sys
+    import select
+    import termios
+
+class KeyboardHook():
+    """Keyboard Hook Class"""
+    def __enter__(self):
+        global IS_WINDOWS
+        if IS_WINDOWS:
+            self.readHandle = GetStdHandle(STD_INPUT_HANDLE)
+            self.readHandle.SetConsoleMode(ENABLE_LINE_INPUT|ENABLE_ECHO_INPUT|ENABLE_PROCESSED_INPUT)
+
+            self.curEventLength = 0
+            self.curKeysLength = 0
+
+            self.capturedChars = []
+        else:
+            # Save the terminal settings
+            self.fd = sys.stdin.fileno()
+            self.new_term = termios.tcgetattr(self.fd)
+            self.old_term = termios.tcgetattr(self.fd)
+
+            # New terminal setting unbuffered
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if IS_WINDOWS:
+            pass
+        else:
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+    def poll(self):
+        """poll method to check for keyboard input"""
+        if IS_WINDOWS:
+            if not len(self.capturedChars) == 0:
+                return self.capturedChars.pop(0)
+
+            events_peek = self.readHandle.PeekConsoleInput(10000)
+
+            if len(events_peek) == 0:
+                return None
+
+            if not len(events_peek) == self.curEventLength:
+                for current_event in events_peek[self.curEventLength:]:
+                    if current_event.EventType == KEY_EVENT:
+                        if ord(current_event.Char) == 0 or not current_event.KeyDown:
+                            pass
+                        else:
+                            curChar = str(current_event.Char)
+                            self.capturedChars.append(curChar)
+                self.curEventLength = len(events_peek)
+
+            if not len(self.capturedChars) == 0:
+                return self.capturedChars.pop(0)
+            else:
+                return None
+        else:
+            data_read, data_write, data_error = select.select([sys.stdin], [], [], 0)
+            if not data_read == []:
+                return sys.stdin.read(1)
+            return None
+
 def main():
     """Main entry point"""
 
@@ -632,75 +705,3 @@ def send_salesforce():
 
 if __name__ == "__main__":
     main()
-
-global isWindows
-
-isWindows = False
-try:
-    from win32api import STD_INPUT_HANDLE
-    from win32console import GetStdHandle, KEY_EVENT, ENABLE_ECHO_INPUT, ENABLE_LINE_INPUT, ENABLE_PROCESSED_INPUT
-    isWindows = True
-except ImportError as e:
-    import sys
-    import select
-    import termios
-
-
-class KeyboardHook():
-    def __enter__(self):
-        global isWindows
-        if isWindows:
-            self.readHandle = GetStdHandle(STD_INPUT_HANDLE)
-            self.readHandle.SetConsoleMode(ENABLE_LINE_INPUT|ENABLE_ECHO_INPUT|ENABLE_PROCESSED_INPUT)
-
-            self.curEventLength = 0
-            self.curKeysLength = 0
-
-            self.capturedChars = []
-        else:
-            # Save the terminal settings
-            self.fd = sys.stdin.fileno()
-            self.new_term = termios.tcgetattr(self.fd)
-            self.old_term = termios.tcgetattr(self.fd)
-
-            # New terminal setting unbuffered
-            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
-            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
-
-        return self
-
-    def __exit__(self, type, value, traceback):
-        if isWindows:
-            pass
-        else:
-            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
-
-    def poll(self):
-        if isWindows:
-            if not len(self.capturedChars) == 0:
-                return self.capturedChars.pop(0)
-
-            eventsPeek = self.readHandle.PeekConsoleInput(10000)
-
-            if len(eventsPeek) == 0:
-                return None
-
-            if not len(eventsPeek) == self.curEventLength:
-                for curEvent in eventsPeek[self.curEventLength:]:
-                    if curEvent.EventType == KEY_EVENT:
-                        if ord(curEvent.Char) == 0 or not curEvent.KeyDown:
-                            pass
-                        else:
-                            curChar = str(curEvent.Char)
-                            self.capturedChars.append(curChar)
-                self.curEventLength = len(eventsPeek)
-
-            if not len(self.capturedChars) == 0:
-                return self.capturedChars.pop(0)
-            else:
-                return None
-        else:
-            dr,dw,de = select.select([sys.stdin], [], [], 0)
-            if not dr == []:
-                return sys.stdin.read(1)
-            return None
