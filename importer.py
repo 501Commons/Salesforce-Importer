84b39341fb38f04a738c268b04ef69642dbef8a6
Dave Boyce <daveb@501commons.org>
2018-10-12 01:32:47 -0700

    update

--- importer.py
+++ importer.py
@@ -315,8 +315,8 @@ def refresh_and_export(importer_directory, salesforce_type,
 
         # Wait for excel to finish refresh
         message = ("Pausing " + str(wait_time) +
-                   " seconds to give Excel time to complete data queries..." +
-                   "\n\t\t***if Excel data queries complete then press any key to exit wait cycle")
+                   " seconds to give Excel time to complete background query..." +
+                   "\n\t\t***if Excel background query complete then press any key to exit wait cycle")
         print message
         refresh_status += message + "\n"
 
@@ -327,8 +327,8 @@ def refresh_and_export(importer_directory, salesforce_type,
 
                     wait_time -= 30
                     message = ("\t" + str(wait_time) +
-                               " seconds remaining for Excel to complete data queries..." +
-                               "\n\t\t***if Excel Refresh complete then press any key to exit wait cycle")
+                               " seconds remaining for Excel to complete background query..." +
+                               "\n\t\t***if Excel background query complete then press any key to exit wait cycle")
                     print message
                     refresh_status += message + "\n"
 
