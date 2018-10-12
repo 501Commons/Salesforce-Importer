14c5c9b21ea0f33952979a958597e172bb35eb64
Dave Boyce <daveb@501commons.org>
2018-10-12 00:02:29 -0700

    keyboard hook

--- importer.py
+++ importer.py
@@ -284,20 +284,28 @@ def refresh_and_export(importer_directory, salesforce_type,
                    " seconds to give Excel time to complete data queries...")
         print message
         refresh_status += message + "\n"
-        while wait_time > 0:
-            if wait_time > 30:
-                time.sleep(30)
 
-                wait_time -= 30
-                message = ("\t" + str(wait_time) +
-                           " seconds remaining for Excel to complete data queries...")
-                print message
-                refresh_status += message + "\n"
+        with KeyboardHook() as keyboard_hook:
+            while wait_time > 0:
+                if wait_time > 30:
+                    time.sleep(30)
 
-            else:
-                time.sleep(wait_time)
-                wait_time = 0
-                break
+                    wait_time -= 30
+                    message = ("\t" + str(wait_time) +
+                               " seconds remaining for Excel to complete data queries...")
+                    print message
+                    refresh_status += message + "\n"
+
+                else:
+                    time.sleep(wait_time)
+                    wait_time = 0
+                    break
+
+                keyboard_input = keyboard_hook.poll()
+                if not keyboard_input is None:
+                    if keyboard_input == "c":
+                        break
+                    print keyboard_input
 
         message = "Refreshing all connections...Completed"
         print message
@@ -624,3 +632,75 @@ def send_salesforce():
 
 if __name__ == "__main__":
     main()
+
+global isWindows
+
+isWindows = False
+try:
+    from win32api import STD_INPUT_HANDLE
+    from win32console import GetStdHandle, KEY_EVENT, ENABLE_ECHO_INPUT, ENABLE_LINE_INPUT, ENABLE_PROCESSED_INPUT
+    isWindows = True
+except ImportError as e:
+    import sys
+    import select
+    import termios
+
+
+class KeyboardHook():
+    def __enter__(self):
+        global isWindows
+        if isWindows:
+            self.readHandle = GetStdHandle(STD_INPUT_HANDLE)
+            self.readHandle.SetConsoleMode(ENABLE_LINE_INPUT|ENABLE_ECHO_INPUT|ENABLE_PROCESSED_INPUT)
+
+            self.curEventLength = 0
+            self.curKeysLength = 0
+
+            self.capturedChars = []
+        else:
+            # Save the terminal settings
+            self.fd = sys.stdin.fileno()
+            self.new_term = termios.tcgetattr(self.fd)
+            self.old_term = termios.tcgetattr(self.fd)
+
+            # New terminal setting unbuffered
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if isWindows:
+            pass
+        else:
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+    def poll(self):
+        if isWindows:
+            if not len(self.capturedChars) == 0:
+                return self.capturedChars.pop(0)
+
+            eventsPeek = self.readHandle.PeekConsoleInput(10000)
+
+            if len(eventsPeek) == 0:
+                return None
+
+            if not len(eventsPeek) == self.curEventLength:
+                for curEvent in eventsPeek[self.curEventLength:]:
+                    if curEvent.EventType == KEY_EVENT:
+                        if ord(curEvent.Char) == 0 or not curEvent.KeyDown:
+                            pass
+                        else:
+                            curChar = str(curEvent.Char)
+                            self.capturedChars.append(curChar)
+                self.curEventLength = len(eventsPeek)
+
+            if not len(self.capturedChars) == 0:
+                return self.capturedChars.pop(0)
+            else:
+                return None
+        else:
+            dr,dw,de = select.select([sys.stdin], [], [], 0)
+            if not dr == []:
+                return sys.stdin.read(1)
+            return None
