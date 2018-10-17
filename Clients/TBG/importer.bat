7998185e5a89510ae07a254b5eb95de23d6b1090
Dave Boyce <daveb@501commons.org>
2018-10-17 13:14:21 -0700

    updates

--- Clients/TBG/importer.bat
+++ Clients/TBG/importer.bat
@@ -8,6 +8,6 @@ set IMPORT_NOEXPORTSF=-noexportsf
 set IMPORT_INSERTATTEMPTS=-insertattempts 1
 set IMPORT_ENABLE_ATTACHMENTS=
 set IMPORT_INTERACTIVEMODE=-interactivemode
-set IMPORT_DISPLAYALERTS=
+set IMPORT_DISPLAYALERTS=-displayalerts
 
 call "%IMPORTER_DIRECTORY%\..\importer.bat" 
