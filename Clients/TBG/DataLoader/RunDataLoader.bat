94b3bcc0a25a4b71198cbf97aaa27e7091594813
Dave Boyce <daveb@501commons.org>
2019-03-19 07:30:52 -0700

    tbg update for new dataloader

--- Clients/TBG/DataLoader/RunDataLoader.bat
+++ Clients/TBG/DataLoader/RunDataLoader.bat
@@ -1,3 +1,3 @@
 echo incoming %1 %2 %3
-cd "C:\Program Files (x86)\salesforce.com\Data Loader\bin"
+cd "%USER_PROFILE%\dataloader\v45.0.0\bin"
 call process C:\repo\Salesforce-Importer-Private\Clients\%2\Salesforce-Importer\Clients\%2\DataLoader\ %3_%1
