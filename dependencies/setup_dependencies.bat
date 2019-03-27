95cf68416e86be5b44cdb7376a76f41bed79f0e0
Dave Boyce <daveb@501commons.org>
2019-03-27 15:26:09 -0700

    updates

--- dependencies/setup_dependencies.bat
+++ dependencies/setup_dependencies.bat
@@ -1,6 +1,6 @@
-set JAVA_HOME=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\Java\jre1.8.0_201
+set JAVA_HOME=C:\repo\Salesforce-Importer\dependencies\Java\jre1.8.0_201
 echo exporter: setting JAVA_HOME to %JAVA_HOME%
-set PYTHON_HOME=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\Python\2.7.16
+set PYTHON_HOME=C:\repo\Salesforce-Importer\dependencies\Python\2.7.16
 echo exporter: setting PYTHON_HOME to %PYTHON_HOME%
-set SF_DATALOADER=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\salesforce.com\dataloader\v44
+set SF_DATALOADER=C:\repo\Salesforce-Importer\dependencies\salesforce.com\dataloader\v44
 echo exporter: setting SF_DATALOADER to %SF_DATALOADER%
