fca38a7f321ab8327c466c18b1688d38e7204a81
Dave Boyce <daveb@501commons.org>
2019-05-17 14:49:06 -0700

    sandbox setup

--- dependencies/setup_dependencies.bat
+++ dependencies/setup_dependencies.bat
@@ -1,6 +1,9 @@
 set JAVA_HOME=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\Java\jre1.8.0_201
 echo exporter: setting JAVA_HOME to %JAVA_HOME%
+
 set PYTHON_HOME=C:\Python27
+set PATH=C:\Python27;C:\Python23\Scripts;%PATH%
 echo exporter: setting PYTHON_HOME to %PYTHON_HOME%
+
 set SF_DATALOADER=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\salesforce.com\dataloader\v44
 echo exporter: setting SF_DATALOADER to %SF_DATALOADER%
