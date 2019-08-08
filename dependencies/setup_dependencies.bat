a7c2ba056a73eaf8c5936642a2b12ffb96c009e5
Dave Boyce <daveb@501commons.org>
2019-08-08 11:39:06 -0700

    fix python path

--- dependencies/setup_dependencies.bat
+++ dependencies/setup_dependencies.bat
@@ -2,7 +2,7 @@ set JAVA_HOME=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesfor
 echo exporter: setting JAVA_HOME to %JAVA_HOME%
 
 set PYTHON_HOME=C:\Python27
-set PATH=C:\Python27;C:\Python23\Scripts;%PATH%
+set PATH=C:\Python27;C:\Python27\Scripts;%PATH%
 echo exporter: setting PYTHON_HOME to %PYTHON_HOME%
 
 set SF_DATALOADER=C:\repo\Salesforce-Importer-Private\Clients\%CLIENT_TYPE%\Salesforce-Importer\dependencies\salesforce.com\dataloader\v44
