1a040024d935eab804aa0ef264edd17d190775ac
Dave Boyce <dboyceman@hotmail.com>
2020-11-07 11:42:43 -0800

    framework for files

---
+++ Clients/II/Execute/Start FFN Import Stages (Prod).bat
@@ -0,0 +1,4 @@
+call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Insert Prod
+call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update1of3 Prod
+call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update2of3 Prod
+call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update3of3 Prod
