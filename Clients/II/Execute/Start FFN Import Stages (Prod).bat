5b05f2ec18d86dd88e74d8dd0b12cb9843a87116
Dave Boyce <dboyceman@hotmail.com>
2020-11-16 13:47:55 -0800

    updates

--- Clients/II/Execute/Start FFN Import Stages (Prod).bat
+++ Clients/II/Execute/Start FFN Import Stages (Prod).bat
@@ -1,4 +1,5 @@
-call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Insert Prod
-call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update1of3 Prod
-call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update2of3 Prod
-call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update3of3 Prod
+call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Insert Prod Cloud
+call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update1of4 Prod Cloud
+call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update2of4 Prod Cloud
+call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update3of4 Prod Cloud
+call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat FFN-Update4of4 Prod Cloud
