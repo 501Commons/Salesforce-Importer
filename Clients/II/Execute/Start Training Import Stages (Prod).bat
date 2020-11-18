331ec047da1c4a258d4da6bf52da459ae8a7231a
Dave Boyce <dboyceman@hotmail.com>
2020-11-18 11:04:38 -0800

    updates

--- Clients/II/Execute/Start Training Import Stages (Prod).bat
+++
@@ -1,2 +0,0 @@
-call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat Training Prod Cloud
-call C:\repo\Salesforce-Importer-Private\Clients\II\importer.bat Training-Update Prod Cloud
--- Clients/IISC/Execute/Start Compass Import Stages (Prod).bat
+++
@@ -1,6 +0,0 @@
-@REM Run SC Importer Insert
-call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat Compass-Insert Prod Cloud
-
-@REM Run SC Importer Update
-call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat Compass-Update1of2 Prod Cloud
-call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat Compass-Update2of2 Prod Cloud
