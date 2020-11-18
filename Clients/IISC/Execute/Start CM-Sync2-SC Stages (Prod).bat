3804b48b283c0217ef87e4c5843965e9a58c9afd
Dave Boyce <dboyceman@hotmail.com>
2020-11-18 11:02:57 -0800

    updates

--- Clients/IISC/Execute/Start CM-Sync2-SC Stages (Prod).bat
+++ Clients/IISC/Execute/Start CM-Sync2-SC Stages (Prod).bat
@@ -4,8 +4,8 @@ set EMAIL_LIST=db.iisc@501commons.org;pauli@imaginewa.org
 call C:\repo\Salesforce-Exporter-Private\Clients\II\exporter.bat Prod %EMAIL_LIST%
 
 @REM Run SC Importer Insert
-REM call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat SYNC-Insert Prod Cloud
+call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat SYNC-Insert Prod Cloud
 
 @REM Run SC Importer Update
-REM call C:\repo\Salesforce-Exporter-Private\Clients\IISC\importer.bat SYNC-Update1of2 Prod Cloud
+call C:\repo\Salesforce-Exporter-Private\Clients\IISC\importer.bat SYNC-Update1of2 Prod Cloud
 call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat SYNC-Update2of2 Prod Cloud
--- Clients/IISC/Execute/Start Compass Import Stages (Prod).bat
+++ Clients/IISC/Execute/Start Compass Import Stages (Prod).bat
@@ -1,6 +1,6 @@
 @REM Run SC Importer Insert
-REM call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat Compass-Insert Prod Cloud
+call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat Compass-Insert Prod Cloud
 
 @REM Run SC Importer Update
-REM call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat Compass-Update1of2 Prod Cloud
+call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat Compass-Update1of2 Prod Cloud
 call C:\repo\Salesforce-Importer-Private\Clients\IISC\importer.bat Compass-Update2of2 Prod Cloud
