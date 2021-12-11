138305d9385d90a39dc18056ccd5c88c93d9942a
daveb-uncruise <daveb@uncruise.com>
2021-12-11 15:07:54 -0800

    up

--- Clients/UCA/importer.bat
+++ Clients/UCA/importer.bat
@@ -7,7 +7,7 @@ set IMPORT_NOEXPORTODBC=-noexportodbc
 set IMPORT_NOEXPORTSF=-noexportsf
 set IMPORT_INSERTATTEMPTS=
 set IMPORT_EMAILATTACHMENTS=-emailattachments
-set IMPORT_INTERACTIVEMODE=
-set IMPORT_DISPLAYALERTS=
+set IMPORT_INTERACTIVEMODE=-interactivemode
+set IMPORT_DISPLAYALERTS=-displayalerts
 
 call "%IMPORTER_DIRECTORY%\..\importer.bat" 
