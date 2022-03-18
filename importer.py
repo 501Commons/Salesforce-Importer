a96f098a767a43ba1a641be46eb4f19325a99652
daveb-uncruise <daveb@uncruise.com>
2022-03-18 06:32:56 -0700

    license

--- importer.py
+++ importer.py
@@ -1,4 +1,4 @@
-""" Nonprofit Salesforce © 2022 by 501 Commons is licensed under CC BY 4.0
+"""Nonprofit Salesforce © 2022 by 501 Commons is licensed under CC BY 4.0"""
 
 """import Module for Excel to Salesforce"""
 
